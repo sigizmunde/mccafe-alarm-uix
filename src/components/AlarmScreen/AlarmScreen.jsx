560b913abaf4245d63ec4da26edbb0b507105e7b
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-07-31 00:49:23 +0300

    feat: slider added

--- src/components/AlarmScreen/AlarmScreen.jsx
+++ src/components/AlarmScreen/AlarmScreen.jsx
@@ -1,4 +1,14 @@
 import RoundClock from 'components/RoundClock/RoundClock';
+import React, { useRef } from 'react';
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  WithStore,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import { useEffect, useState } from 'react';
 import { fetchDrinks } from 'services/api-mockup';
 
@@ -35,9 +45,11 @@ const Main = styled.main`
 `;
 
 const Caption = styled.p`
+  margin: 0;
   font-style: normal;
   font-weight: 400;
   font-size: 18px;
+  line-height: 1.2;
 `;
 
 const Digits = styled.span`
@@ -46,6 +58,22 @@ const Digits = styled.span`
   font-weight: 400;
   font-size: 72px;
   line-height: 1;
+`;
+
+const ClockBlock = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ClockContainer = styled.div`
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
 `;
 
 const CircleDiv = styled.div`
@@ -58,28 +86,131 @@ const CircleDiv = styled.div`
 
 const CoffeeImg = styled.img`
   display: block;
+  margin: 0 auto;
   width: 92%;
 `;
+
+const prevButtonStyle = {
+  position: 'absolute',
+  zIndex: 1,
+  left: '0',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  backgroundColor: 'transparent',
+  color: 'var(--text-color)',
+  fontFamily: 'var(--font-family-monda)',
+  fontSize: '42px',
+  border: 'none',
+};
+
+const nextButtonStyle = {
+  position: 'absolute',
+  zIndex: 1,
+  right: '0',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  backgroundColor: 'transparent',
+  color: 'var(--text-color)',
+  fontFamily: 'var(--font-family-monda)',
+  fontSize: '42px',
+  border: 'none',
+};
+
+const sliderStyle = {
+  width: '360px',
+  height: '360px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '0 auto',
+};
+
+const slideStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  margin: '0',
+};
+
+class ComponentToGetCarouselProps extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  componentDidUpdate() {
+    console.log(this.props);
+    const x = this.props.currentSlide;
+    if (this.props.updateIndex && x) this.props.updateIndex(x);
+    if (x) localStorage.setItem('coffeeIndex', x);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+const MyWithStore = WithStore(ComponentToGetCarouselProps, state => ({
+  currentSlide: state.currentSlide,
+}));
 
 const AlarmScreen = () => {
   const [alarmTime, setAlarmTime] = useState(new Date());
   const [items, setItems] = useState([]);
-  const [index, setIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(
+    localStorage.getItem('coffeeIndex') || 0
+  );
 
   useEffect(() => {
     fetchDrinks().then(response => setItems(response));
   }, []);
 
+  const writeCurrentRef = x => {
+    // currentIndex.current = x;
+    setCurrentIndex(x);
+    console.log(x);
+  };
+
   return (
     <Container>
       <MenuHeader></MenuHeader>
       <Main>
-        <RoundClock value={alarmTime} />
-        {items.length === 0 && <CircleDiv />}
         {items.length > 0 && (
-          <CoffeeImg src={items[index].img_src} alt={items[index].name} />
+          <>
+            <Caption>Your drink is</Caption>
+            <Caption>{items[currentIndex]?.name}</Caption>
+          </>
         )}
+        <ClockBlock>
+          <ClockContainer>
+            <RoundClock value={alarmTime} />
+          </ClockContainer>
+          {items.length === 0 && <CircleDiv />}
 
+          {items.length > 0 && (
+            <CarouselProvider
+              naturalSlideWidth={100}
+              naturalSlideHeight={100}
+              totalSlides={items.length}
+              // currentSlide={localStorage.getItem('coffeeIndex') || 0}
+              infinite={true}
+              isIntrinsicHeight={true}
+            >
+              <ButtonBack style={prevButtonStyle}>{'<'}</ButtonBack>
+              <Slider style={sliderStyle}>
+                {items.map(({ id, img_src, name }, index) => {
+                  return (
+                    <Slide index={index} key={id} style={slideStyle}>
+                      <CoffeeImg src={img_src} alt={name} />
+                    </Slide>
+                  );
+                })}
+              </Slider>
+              <ButtonNext style={nextButtonStyle}>{'>'}</ButtonNext>
+              <MyWithStore updateIndex={writeCurrentRef} />
+            </CarouselProvider>
+          )}
+        </ClockBlock>
         <Caption>Coffee time:</Caption>
         <Digits>
           {alarmTime.getHours().toString().padStart(2, '0') +
--- src/components/RoundClock/RoundClock.jsx
+++ src/components/RoundClock/RoundClock.jsx
@@ -2,7 +2,17 @@ import './RoundClock.css';
 import Clock from 'react-clock';
 
 const RoundClock = ({ value }) => {
-  return <Clock value={value} />;
+  return (
+    <Clock
+      value={value}
+      size={310}
+      minuteHandWidth={6}
+      hourHandWidth={9}
+      renderSecondHand={false}
+      renderMinuteMarks={false}
+      renderHourMarks={false}
+    />
+  );
 };
 
 export default RoundClock;
